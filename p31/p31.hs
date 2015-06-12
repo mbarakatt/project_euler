e003f4218451a344f3753d8ab59998db9600c4e3
mbarakatt <mbarakatt@gmail.com>
2015-06-12 16:04:55 -0400

    Needed a main for some reason
    Should compile without problem now.

--- p31/p31.hs
+++ p31/p31.hs
@@ -1,9 +1,10 @@
 import Data.MemoCombinators
 
+
 p=[200,100,50,20,10,5,2,1]
 
 f2 ([]) _ _ = []
-f2 (h:t) a b = f (a-h) (minimum [h,a-h]) :f2 t a b      
+f2 (h:t) a b = f (a-h) (minimum [h,a-h]) :f2 t a b
 
 f ::  Int -> Int -> Integer
 f = memo2 integral integral f'
@@ -11,4 +12,6 @@ f = memo2 integral integral f'
     f' a 1 = 1
     f' a b = (if  a==b && a `elem` p then 1 else 0 ) + sum (f2 [c | c <- p, c<=b] a b )
 
-prob31 a = f a a 
+prob31 a = f a a
+
+main = putStrLn $ show $ prob31 200
